8663080b8bb7ec77a42b8ecaab84f628a63fa81f
Neelcd <neelpatel0806@gmail.com>
2022-09-12 02:18:45 +0530

    Change

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
-  setupFiles: ["<rootDir>/script/test-setup.js"],
+  // setupFiles: ["<rootDir>/script/test-setup.js"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
 module.exports = createJestConfig(customJestConfig);
